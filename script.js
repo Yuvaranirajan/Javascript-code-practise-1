87ca11578823a589eef343ad03bb51025906c9ae
Yuvaranirajan <yuvarani052020@gmail.com>
2024-02-05 14:23:55 +0530

    call,apply,bind function

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ const obj={
     }
 };
 obj.x();
+
+// call , apply , bind function
+
+let person={
+    firstname:"yuva",
+    lastname:"Rani"
+};
+let person2={
+    firstname:"Abdul",
+    lastname:"kalam",
+    name:function (){
+        console.log(this.firstname,this.lastname);
+    }
+};
+function fullname(h,he){
+    console.log(this.firstname,this.lastname,h,he);
+}
+function name(){
+    console.log(this.firstname,this.lastname);
+}
+ // call function
+name.call(person);
+
+// apply function
+fullname.apply(person,["hi","hello"]);
+
+// bind function
+let info=name.bind(person2);
+info();
